fc9450904d9da5dbbe7e18423886418005e4ced6
Thomas-Georges <thomas.georges.2015@protonmail.com>
2025-03-21 07:20:05 +0100

    Complete refactoring of the scripts in the shape of a new folder called STELLAR-NEW

---
+++ STELLAR-NEW/Training/Scripts/run_render_all.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dynamics import chaser_discrete
+import os
+from visualizer_all import render_visual
+from time import sleep
+os.chdir('runs')
+
+
+typegraph = input('(vbar{n}): ')
+path = os.getcwd()
+#path = '/Users/thomas/Desktop/MSc_IA/Lab_Project/ai_oos/STELLAR-COPY/runs'
+print(path)
+data_dir = os.path.join(path, typegraph)
+
+num = len(os.listdir(data_dir))
+
+if num > 0:
+    num -= 1
+
+#data_file_name = f'chaser{num}.txt'
+
+only_x0 = str(input('Render only inital States [Y] [N]: '))
+#data_dir = 'runs', only_inital = False, terminal_plots = False, run_file = None
+if only_x0 == 'Y':
+    vis_obj = render_visual(data_dir = typegraph, only_inital = True, terminal_plots = False, run_file = None)
+else:
+    vis_obj = render_visual(data_dir = typegraph, only_inital = False, terminal_plots = False, run_file = None)
+vis_obj.render_animation()
+vis_obj.save()
